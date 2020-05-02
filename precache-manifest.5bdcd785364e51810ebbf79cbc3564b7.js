2440e473dcc4eead39b46eee24f2a01ea36eb474
Anurag Pal <apal895@gmail.com>
2020-05-02 13:26:40 +0530

    Updates

---
+++ precache-manifest.5bdcd785364e51810ebbf79cbc3564b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7948bd03f2269a2570a347161d77b6bf",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "7c2bfe93510e552b0271",
+    "url": "/corona-tracker/static/css/main.7a415f08.chunk.css"
+  },
+  {
+    "revision": "67b8eaec895a0c93dc31",
+    "url": "/corona-tracker/static/js/2.9e022dd3.chunk.js"
+  },
+  {
+    "revision": "98e7206d2472a9ba0c2c6593c9913f5f",
+    "url": "/corona-tracker/static/js/2.9e022dd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c2bfe93510e552b0271",
+    "url": "/corona-tracker/static/js/main.b3a06a7b.chunk.js"
+  },
+  {
+    "revision": "91359a7992344710d3f3",
+    "url": "/corona-tracker/static/js/runtime-main.b32a8805.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/covid.d7265326.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39e94eef56d30b1fe1dba5661a310130.js"
+  "/corona-tracker/precache-manifest.5bdcd785364e51810ebbf79cbc3564b7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/corona-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
